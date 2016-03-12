531c25df5dd65dbaa84f488715ad489aac7411fc
Kelvin Hermawan Leonardo <hermawan_kelvin@yahoo.com>
2016-03-12 17:08:57 +0700

    update dikit-dikit tgl 12-03-2016

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -38,7 +38,67 @@ module.exports = {
 				var file2 = "";
 				var file3 = "";
 				var file4 = "";
-				if(typeof req.param('file_url_1')=="undefined") {
+				// if(typeof req.param('file_url_1')=="undefined") {
+				// 	var info = ['Anda harus mengupload akte lahir anda']
+				// 	 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
+				// 	 // the key of usernamePasswordRequiredError
+				// 	 req.session.flash = {
+				// 		 err: info,
+				// 	 }
+				// 	 res.redirect('/user/dashboard');
+				// 	 return;
+				// }
+				// if(typeof req.param('file_url_2')=="undefined") {
+				// 	var info = ['Anda harus mengupload ijazah anda']
+				// 	 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
+				// 	 // the key of usernamePasswordRequiredError
+				// 	 req.session.flash = {
+				// 		 err: info,
+				// 	 }
+				// 	 res.redirect('/user/dashboard');
+				// 	 return;
+				// }
+				// if(typeof req.param('file_url_3')=="undefined") {
+				// 	var info = ['Anda harus mengupload dokumen pendukung 1 anda']
+				// 	 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
+				// 	 // the key of usernamePasswordRequiredError
+				// 	 req.session.flash = {
+				// 		 err: info,
+				// 	 }
+				// 	 res.redirect('/user/dashboard');
+				// 	 return;
+				// }
+				// if(typeof req.param('file_url_4')=="undefined") {
+				// 	var info = ['Anda harus mengupload dokumen pendukung 2 anda']
+				// 	 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
+				// 	 // the key of usernamePasswordRequiredError
+				// 	 req.session.flash = {
+				// 		 err: info,
+				// 	 }
+				// 	 res.redirect('/user/dashboard');
+				// 	 return;
+				// }
+				// if(typeof req.param('file_url_1')!="undefined") {
+				// 		var fs = require('fs');
+				// 		file1 = req.param('file_url_1');
+				// 		fs.write(new Buffer (file1));
+				// }
+				// if(typeof req.param('file_url_2')!="undefined") {
+				// 		var fs = require('fs');
+				// 		file2 = req.param('file_url_2');
+				// 		fs.write(new Buffer (file2));
+				// }
+				// if(typeof req.param('file_url_3')!="undefined") {
+				// 		var fs = require('fs');
+				// 		file3 = req.param('file_url_3');
+				// 		fs.write(new Buffer (file3));
+				// }
+				// if(typeof req.param('file_url_4')!="undefined") {
+				// 		var fs = require('fs');
+				// 		file4 = req.param('file_url_4');
+				// 		fs.write(new Buffer (file4));
+				// }
+				if(typeof req.param('file_name_1')=="undefined") {
 					var info = ['Anda harus mengupload akte lahir anda']
 					 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
 					 // the key of usernamePasswordRequiredError
@@ -48,7 +108,7 @@ module.exports = {
 					 res.redirect('/user/dashboard');
 					 return;
 				}
-				if(typeof req.param('file_url_2')=="undefined") {
+				if(typeof req.param('file_name_2')=="undefined") {
 					var info = ['Anda harus mengupload ijazah anda']
 					 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
 					 // the key of usernamePasswordRequiredError
@@ -58,7 +118,7 @@ module.exports = {
 					 res.redirect('/user/dashboard');
 					 return;
 				}
-				if(typeof req.param('file_url_3')=="undefined") {
+				if(typeof req.param('file_name_3')=="undefined") {
 					var info = ['Anda harus mengupload dokumen pendukung 1 anda']
 					 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
 					 // the key of usernamePasswordRequiredError
@@ -68,7 +128,7 @@ module.exports = {
 					 res.redirect('/user/dashboard');
 					 return;
 				}
-				if(typeof req.param('file_url_4')=="undefined") {
+				if(typeof req.param('file_name_4')=="undefined") {
 					var info = ['Anda harus mengupload dokumen pendukung 2 anda']
 					 // Remember that err is the object being passed down (a.k.a. flash.err), whose value is another object with
 					 // the key of usernamePasswordRequiredError
@@ -78,23 +138,18 @@ module.exports = {
 					 res.redirect('/user/dashboard');
 					 return;
 				}
-				if(typeof req.param('file_url_1')!="undefined") {
-						file1 = req.param('file_url_1');
+				if(typeof req.param('file_name_1')!="undefined") {
+						file1 = req.param('file_name_1');
 				}
-				if(typeof req.param('file_url_2')!="undefined") {
-						file2 = req.param('file_url_2');
+				if(typeof req.param('file_name_2')!="undefined") {
+						file2 = req.param('file_name_2');
 				}
-				if(typeof req.param('file_url_3')!="undefined") {
-						file3 = req.param('file_url_3');
+				if(typeof req.param('file_name_3')!="undefined") {
+						file3 = req.param('file_name_3');
 				}
-				if(typeof req.param('file_url_4')!="undefined") {
-						file4 = req.param('file_url_4');
+				if(typeof req.param('file_name_4')!="undefined") {
+						file4 = req.param('file_name_4');
 				}
-				// var file = [];
-				// file.push(file1);
-				// file.push(file2);
-				// file.push(file3);
-				// file.push(file4);
 				var usrObj = {
 						grade : req.param('grade'),
 						name : req.param('name'),
@@ -107,7 +162,7 @@ module.exports = {
 						file1 : file1,
 						file2 : file2,
 						file3 : file3,
-						file4 : file4,
+						file4 : file4
 				}
 				User.update(req.session.User.id,usrObj,function(err,user){
 						if(err) return next(err);
