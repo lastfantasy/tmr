ad0d76ba85079a9c253ae3d97e7d120b18f6fe23
Christian Yaputra <christianyaputra@hotmail.com>
2016-06-01 15:31:06 +0700

    buang dikit yg g perlu

--- tmr-mobile/www/js/controllers.js
+++ tmr-mobile/www/js/controllers.js
@@ -19,11 +19,7 @@ angular.module('starter')
 
 .controller('AppCtrl', function($scope, $ionicModal) {
   
-  $scope.contacts = [
-    { name: 'Gordon Freeman' },
-    { name: 'Barney Calhoun' },
-    { name: 'Lamarr the Headcrab' },
-  ];
+  
 
   $ionicModal.fromTemplateUrl('templates/modal.html', {
     scope: $scope
@@ -31,10 +27,7 @@ angular.module('starter')
     $scope.modal = modal;
   });
   
-  $scope.createContact = function(u) {        
-    $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
-    $scope.modal.hide();
-  };
+  
 
 })
 
