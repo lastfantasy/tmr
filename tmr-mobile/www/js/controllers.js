762201665e24b1e5b8e3feb1dcd50d0ef1b0b1cf
Christian Yaputra <christianyaputra@hotmail.com>
2016-06-01 15:25:12 +0700

    tambah modal profil

--- tmr-mobile/www/js/controllers.js
+++ tmr-mobile/www/js/controllers.js
@@ -17,6 +17,27 @@ angular.module('starter')
     }
 })
 
+.controller('AppCtrl', function($scope, $ionicModal) {
+  
+  $scope.contacts = [
+    { name: 'Gordon Freeman' },
+    { name: 'Barney Calhoun' },
+    { name: 'Lamarr the Headcrab' },
+  ];
+
+  $ionicModal.fromTemplateUrl('templates/modal.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+  
+  $scope.createContact = function(u) {        
+    $scope.contacts.push({ name: u.firstName + ' ' + u.lastName });
+    $scope.modal.hide();
+  };
+
+})
+
 .controller('DaftarCtrl', function($scope, $http, $ionicLoading, $state) {
     var obj = {};
     $scope.message = "";
