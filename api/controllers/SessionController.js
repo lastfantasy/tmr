f5eef6228ff62f8a9208d770878e588d10951aab
Kelvin Hermawan Leonardo <hermawan_kelvin@yahoo.com>
2016-07-22 16:31:35 +0700

    Documents sudah normal

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -64,7 +64,7 @@ module.exports = {
 				email : req.param('email'),
 				types : "1",
 				name : "",
-				files : [4],
+				files : [],
 				grade : "",
 				gender: "",
 		    	address : '',
@@ -89,14 +89,14 @@ module.exports = {
                     req.session.flash = {
                       success: requireLoginError
                     }
-                    user.files[0].name = "";
-                    user.files[0].url = "";
-                    user.files[1].name = "";
-                    user.files[1].url = "";
-                    user.files[2].name = "";
-                    user.files[2].url = "";
-                    user.files[3].name = "";
-                    user.files[3].url = "";
+                    // user.files[0].name = "";
+                    // user.files[0].url = "";
+                    // user.files[1].name = "";
+                    // user.files[1].url = "";
+                    // user.files[2].name = "";
+                    // user.files[2].url = "";
+                    // user.files[3].name = "";
+                    // user.files[3].url = "";
                     res.redirect('/login');
                     return;
 								});
