2579797861170ca7d4fa12f7f0ab23e358e9f701
Kelvin Hermawan Leonardo <hermawan_kelvin@yahoo.com>
2016-07-23 23:09:10 +0700

    Usaha buat tampilan open close tanpa login (tapi gagal)

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -5,9 +5,19 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var _opendate, _closedate;
 module.exports = {
 	index : function(req,res,next){
-		return res.view();
+		User.findOne({admin : true}, function(err, user){
+			_opendate = new Date(user.opendate);
+			_closedate = new Date(user.closedate);
+			console.log(_opendate);
+			console.log(_closedate);
+			return res.view('homepage.ejs');
+		});
+		// console.log(opendate);
+		// console.log(closedate);
+		// return res.view('homepage.ejs');
 	}
 };
 
